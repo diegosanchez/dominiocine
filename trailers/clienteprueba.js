61946623ba413e3c37b2a50ba28b618f7456d11f
cursonodeuser003 <maximiliano.bordon@gmail.com>
2015-02-10 23:10:42 +0000

    Version estable

--- trailers/clienteprueba.js
+++ trailers/clienteprueba.js
@@ -1,13 +1,19 @@
-var trailer = require("./index");
-var fs = require("fs");
 var mongojs = require('mongojs');
-var db = mongojs('cinema26', ['films']);
-var instance = new trailer();
-
-instance.search(function(peliculas) {
-                                          console.log("Procesando");
+var db = mongojs('cinema32', ['films']);
+var trailer = require("./index.js")(db);
+var fs = require("fs");
 
 
+
+console.log(trailer.search);
+trailer.search(function(peliculas) {
+                                        console.log(peliculas[0].trailers); 
+										/*for (var index=0;index<peliculas.length;index++)
+                                        {
+										         console.log(peliculas[index].trailers);
+										}*/
+                                         
+                                          										
 
                  
 });
