881a271da49509886043e8bac962d63387867e07
Cynthia Chibana <chibana.cynthia@gmail.com>
2015-02-12 23:39:47 +0000

    books

--- books/index.js
+++ books/index.js
@@ -1,24 +1,18 @@
-/*
-	Crear un modulo que busque por cada pelicula el o los libros relacionados.
-	La busqueda sera en google books.
-	Actualizar cada item de la collection films con una propiedad llamada books que será igual a un
-	array que contendrá en cada posicion un json con la información de cada libro.
-	Marcar cada película procesada con la propiedad booked = true.
-*/
 var https = require('https');
 var Events = require('events').EventEmitter;
 var Emitter = new Events();
-var mongojs = require('mongojs');
-var db = mongojs('dominocine', ['films']);
 
-module.exports = function(){
+module.exports = function(mongodb){
 
+	var cursor;
+	var movies;
 	var _this = this;
+	this.mongodb = mongodb;
 	
-	this.getBooks = function(movieName, lang, cb){
+	var getBooks = function(movieTitle, lang, cb){
 
 		var body = "";
-		var link = "https://www.googleapis.com/books/v1/volumes?q="+movieName+"&langRestrict="+lang+"&fields=items(id,selfLink,volumeInfo)";
+		var link = "https://www.googleapis.com/books/v1/volumes?q="+movieTitle+"&langRestrict="+lang+"&fields=items(id,selfLink,volumeInfo)";
 
 		https.get(link, function(res){
 			if(res.statusCode==200){
@@ -34,45 +28,45 @@ module.exports = function(){
 			}
 		});//end http.get
 	}//end getBooks	
+
+	Emitter.on("next", function(movie){
 	
-	this.actualizarDBLibros = function(){
+		getBooks(movie.title, "en", function(books){
+			if(books != undefined)
+				mongodb.films.update({title:movie.title},{$set: { books:books, book:true }}, {multi:true}, function(err, docs){
+					if(err){
+						console.log("Pelicula: "+movie.title+" >> Books >> Error al hacer update en la base");
+					}else{
+						console.log("Pelicula: "+movie.title+" >> Books >> Libros cargados", docs);
+					}
+				});
+			else 
+				mongodb.films.update({title:movie.title},{$set: { books:[], book:true }}, {multi:true}, function(err, docs){
+					if(!err)
+						console.log("Pelicula: "+movie.title+" >> Books >> No se encontraron libros relacionados", docs);
+				});
+			$next();
+		});//end getBook
+          
+    });
 	
-		var cursor = 0;
-		var movies;
+	
+	var $next = function(){
+		if(cursor===movies.length){
+			return false;
+		}else{
+			var $return = movies[cursor++];
+            Emitter.emit("next", $return);
+			return $return;
+		}
+	};//end $next
+	
+	var actualizarDBLibros = function(){
+		cursor = 0;
 	
 		console.log("Actualizando libros en DB");
 		
-		var $next=function(){
-			if(cursor===movies.length){
-				return false;
-			}else{
-				var $return=  movies[cursor++];
-				return $return;
-			}
-		};//end $next
-		
-		var updateInfoBookDB = function(movieTitle){
-			_this.getBooks(movieTitle, "en", function(books){
-				if(books != undefined)
-					db.films.update({title:movieTitle},{$set: { books:books, book:true }}, {multi:true}, function(err, docs){
-						if(err){
-							console.log("Pelicula: "+movieTitle+" >> Books >> Error al hacer update en la base");
-						}else{
-							console.log("Pelicula: "+movieTitle+" >> Books >> Libros cargados", docs);
-						}
-					});
-				else 
-					db.films.update({title:movieTitle},{$set: { books:[], book:true }}, {multi:true}, function(err, docs){
-						if(!err)
-							console.log("Pelicula: "+movieTitle+" >> Books >> No se encontraron libros relacionados", docs);
-					});
-				var nextBook = $next();
-				if(nextBook != false)
-					updateInfoBookDB(nextBook.title);
-			});//end getBook
-		};//end updateInfoBookDB
-		
-		db.films.find({book:false},{book:1, title:1},{limit:10},function(err, docs){
+		mongodb.films.find({book:false},{book:1, title:1},{limit:10}, function(err, docs){
             if(err){
                 console.log("Error busqueda en db libros");
             } else{
@@ -81,11 +75,15 @@ module.exports = function(){
                 }else{
 					//console.log(docs.length);
 					movies = docs;
-					updateInfoBookDB(movies[0].title);
+					$next();
                 }//end else
             }//end else
-        });//end db.films.find
+        });//end mongodb.films.find
 		
 	}//end actualizarDBLibros
+	
+	actualizarDBLibros();
+    setInterval(actualizarDBLibros, 15*(60*1000));
+	
 }//end module.exports	
 
