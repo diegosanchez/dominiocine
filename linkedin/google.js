404f13e8c203ebccfef21d3d61fc794546231888
diego sanchez <diegos@taohome.(none)>
2015-02-16 09:31:34 -0300

    Config vars added

--- linkedin/google.js
+++ linkedin/google.js
@@ -1,16 +1,19 @@
+var config = require('config');
 var request = require('request');
 var util = require('util');
 
 var url = "https://www.googleapis.com/customsearch/v1";
-var key = "***************************************";
-var cx = "*********************:+++++++++++";
+var key = config.get('linkedin.google_key');
+var cx = config.get('linkedin.searche_key');
 var q = "cameron diaz";
 
 var uri = util.format("%s?key=%s&cx=%s&q=%s", 
     url, key, cx, encodeURIComponent(q) );
 
+console.log("Requesting: ", uri );
+
 request( uri,
 function(err, income, body) {
   var response = JSON.parse(body);
-  console.log(response.items[0].link);
+  console.log(response);
 });
